d5e85658c5054205ad8ebd636624d05f759812c5
Viraz <virajm72@gmail.com>
2019-12-10 23:55:31 +0530

    Test Cases complete with a few contract modifications

--- src/contracts/Marvel.sol
+++ src/contracts/Marvel.sol
@@ -7,10 +7,20 @@ contract Marvel is ERC721Full {
 
 uint256 id;
 address owner;
+string[] initials;
 
 constructor() public ERC721Full("Marvel", "MARVEL") {
    owner = msg.sender;
 }
+modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+event Minted(
+    string initials,
+    string colour
+);
 
 struct character{
     string initials;
@@ -21,10 +31,22 @@ struct character{
 mapping(uint256 => character) public marvelAvatar;
 
 
-function mint(string memory _initials, string memory _color) public {
+function mint(string memory _initials, string memory _color) public onlyOwner {
+     require(!isExistingToken(_initials), "No Duplicate Tokens Allowed");
      marvelAvatar[id] = character(_initials, _color, false);
      _mint(msg.sender, id);
      id = id + 1;
      marvelAvatar[id].isMinted = true;
+     initials.push(_initials);
+     emit Minted(_initials, _color);
+}
+
+function isExistingToken(string memory _initials) internal returns (bool) {
+    for(uint256 i = 0; i < initials.length; i++){
+       if(keccak256(abi.encodePacked((initials[i]))) == keccak256(abi.encodePacked((_initials)))) {
+            return true;
+        }
+    }
+    return false;
 }
 }
